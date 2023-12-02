046a7c985dd39eba19fd6555a0ca1f67d2c6cb95
th112 <th112@HIRAHIRAPC>
2023-12-02 21:01:56 +0900

    プロジェクト ファイルを追加します。

---
+++ BloggingContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreSample
+{
+    internal class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=Blogging;Trusted_Connection=True");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using EFCoreSample;
+
+using (var db = new BloggingContext())
+{
+    // 登録
+    db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+    db.SaveChanges();
+
+    // 読み取り
+    var blog = db.Blogs.OrderBy(b => b.BlogId).First();
+    Console.WriteLine($"ブログを新規登録しました！　" + $"ブログID：{blog.BlogId}, ブログURL：{blog.Url}");
+
+    // 更新
+    blog.Url = "https://devblogs.microsoft.com/dotnet";
+    blog.Posts.Add(new Post { Title = "Hello World", Content = "EF CoreとPostgreSQL" });
+    db.SaveChanges();
+    Console.WriteLine($"ブログを更新しました！　" + $"ブログID：{blog.BlogId}, ブログURL：{blog.Url}");
+    Console.WriteLine($"投稿タイトル：{blog.Posts.First().Title}, " + $"投稿内容：{blog.Posts.First().Content}");
+
+    // 削除
+    Console.WriteLine("ブログを削除しました！");
+    db.Remove(blog);
+    db.SaveChanges();
+}
