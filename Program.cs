396a7c00dd2b47838228136c68683ace9d646d66
hirahira <th11282@gmail.com>
2023-12-03 16:55:29 +0900

    DIの設定とCRUD処理の移動

--- Program.cs
+++ Program.cs
@@ -1,24 +1,29 @@
 ﻿using EFCoreSample;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-using (var db = new BloggingContext())
-{
-    // 登録
-    db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
-    db.SaveChanges();
+var builder = Host.CreateApplicationBuilder(args);
 
-    // 読み取り
-    var blog = db.Blogs.OrderBy(b => b.BlogId).First();
-    Console.WriteLine($"ブログを新規登録しました！　" + $"ブログID：{blog.BlogId}, ブログURL：{blog.Url}");
+// DbContextをDIに登録
+builder.Services.AddDbContext<BloggingContext>();
 
-    // 更新
-    blog.Url = "https://devblogs.microsoft.com/dotnet";
-    blog.Posts.Add(new Post { Title = "Hello World", Content = "EF CoreとPostgreSQL" });
-    db.SaveChanges();
-    Console.WriteLine($"ブログを更新しました！　" + $"ブログID：{blog.BlogId}, ブログURL：{blog.Url}");
-    Console.WriteLine($"投稿タイトル：{blog.Posts.First().Title}, " + $"投稿内容：{blog.Posts.First().Content}");
+// サービスをDIに登録
+builder.Services.AddSingleton<BlogService>();
 
-    // 削除
-    Console.WriteLine("ブログを削除しました！");
-    db.Remove(blog);
-    db.SaveChanges();
-}
+// 登録したサービスを呼び出す
+var provider = builder.Services.BuildServiceProvider();
+var blogService = provider.GetService<BlogService>();
+
+// 登録
+await blogService.Insert(new Blog { Url = "aaa.com", Rating = 10 });
+
+// 更新
+await blogService.Update(new Blog { BlogId = 2, Url = "bbb.com", Rating = 4 });
+
+// 削除
+await blogService.Delete(2);
+
+// 読み取り
+await blogService.GetAll();
+
+Console.ReadLine();
