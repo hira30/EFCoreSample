3c19800d7665308894a2ef2b7bbb2c4df0df8994
hirahira <th11282@gmail.com>
2024-02-14 09:13:34 +0900

    SQLite用プロジェクトの追加

---
+++ SQLiteCrudSample/BloggingContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SQLiteCrudSample
+{
+    internal class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public string DbPath { get; }
+
+        public BloggingContext()
+        {
+            // 特殊フォルダ（デスクトップ）の絶対パスを取得
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            // DBファイルの保存先とDBファイル名をDbPathに格納
+            DbPath = $"{path}{Path.DirectorySeparatorChar}blogging.db";
+        }
+
+        // デスクトップ上にSQLiteのDBファイルが作成される
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
