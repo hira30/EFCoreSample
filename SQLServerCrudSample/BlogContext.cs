e4c434de8577aa0132eb961e85537e7f7d046980
hirahira <th11282@gmail.com>
2024-02-17 19:21:35 +0900

    BlogContextの追加

---
+++ SQLServerCrudSample/BlogContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+namespace SQLServerCrudSample
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .LogTo(Console.WriteLine, LogLevel.Information, DbContextLoggerOptions.LocalTime)
+                .UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=Blog;Trusted_Connection=True");
+        }
+    }
+
+    // Blog : Post = 1 : N
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string? Url { get; set; }
+        public int Rating { get; set; }
+
+        //　ナビゲーションプロパティ（Entity間のリレーションを表す）
+        public List<Post> Posts { get; set; } = [];
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        
+        // 外部キー
+        public int BlogId { get; set; }
+        // ナビゲーションプロパティ
+        public Blog Blog { get; set; } = new();
+    }
+}
--- SQLServerCrudSample/TodoContext.cs
+++ SQLServerCrudSample/TodoContext.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 
 namespace SQLServerCurdSample
 {
@@ -8,8 +11,21 @@ namespace SQLServerCurdSample
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(
-                @"Server=(localdb)\mssqllocaldb;Database=Sample;Trusted_Connection=True");
+            optionsBuilder
+                .LogTo(Console.WriteLine,  LogLevel.Information, DbContextLoggerOptions.LocalTime)
+                .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Todo;Trusted_Connection=True");
         }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder
+        //        .LogTo(
+        //            Console.WriteLine,
+        //            //message => Debug.WriteLine(message),
+        //            LogLevel.Information,
+        //            DbContextLoggerOptions.LocalTime)
+        //        //.EnableSensitiveDataLogging()
+        //        .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Todo;Trusted_Connection=True");
+        //}
     }
 }
